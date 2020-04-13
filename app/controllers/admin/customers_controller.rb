16ce4d0cda39c13e2cae3d3ba6284249059a6147
teru75 <love.akg.75@gmail.com>
2020-04-13 14:30:28 +0000

    [add]admin/customer index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,14 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(parmas[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
