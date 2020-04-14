31a9c28d504f34612ed83bddfb0cab9c1d8590d8
teru75 <love.akg.75@gmail.com>
2020-04-14 06:43:53 +0000

    [add]event-newpage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,20 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
+    @menu = @event.menus.build
+    @reps = @menu.reps.build
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.customer_id = current_customer.id
+
+    if @event.save
+      flash[:success] = "トレーニングを保存しました！"
+       redirect_to event_path(@event)
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,4 +31,17 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def event_params
+    params.require(:event).permit(:part, :start, :end,
+        menus_attributes:
+        [
+        :id, :name, :_destroy, reps_attributes:
+        [:id, :weight, :count, :set_count, :_destroy
+        ]
+      ]
+      )
+  end
+
 end
