a4e464896900b6cad291c1aca2a927685e5a5c04
teru75 <love.akg.75@gmail.com>
2020-04-15 16:27:07 +0000

    [update]datapage data into chart

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -1,19 +1,19 @@
 class DatasController < ApplicationController
   def index
+    if params[:menu] == "1"
+    @menu = "ベンチプレス"
+    end
     @customer = Customer.find(current_customer.id)
-    @customer.events.each do |event|
+    @dayline = Array.new
+    @timeline = Array.new
+    count = 0
+    @events = @customer.events.joins(:menus).where("menus.name = 'ベンチプレス'").order(start: "ASC").last(5)
+    @events.each do |event|
       event.menus.each do |menu|
-        if menu.name == "ベンチプレス"
-          @graphdays =  @customer.events.order(start: "DESC").limit(6).reverse
-          @dayline = Array.new
-          @graphdays.each do |graphday|
-              @dayline.push(graphday.start.strftime('%m/%d').to_s)
-          end
-          @graphtimes =  menu.reps.order(weight: "DESC").limit(6).reverse
-          @timeline = Array.new
-          @graphtimes.each do |graphtime|
-            @timeline.push(graphtime.weight)
-          end
+        if menu.name == @menu && count < 5
+          count+= 1
+          @dayline.push(menu.event.start.strftime('%m/%d').to_s)
+          @timeline.push(menu.reps.order(weight: "DESC").first[:weight])
         end
       end
     end
