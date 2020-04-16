97c2a83ab32a0bee1fd0f7d330542b214e46a902
teru75 <love.akg.75@gmail.com>
2020-04-16 08:51:22 +0000

    [add]my_menu_likes

--- app/controllers/my_menu_likes_controller.rb
+++ app/controllers/my_menu_likes_controller.rb
@@ -1,11 +1,17 @@
 class MyMenuLikesController < ApplicationController
 
-def create
-  
-end
+  def create
+    @my_menu = MyMenu.find(params[:my_menu_id])
+    like = current_customer.my_menu_likes.new(my_menu_id: @my_menu.id)
+    like.save
+    redirect_to request.referer
+  end
 
-def destroy
-  
-end
+  def destroy
+    @my_menu = MyMenu.find(params[:my_menu_id])
+    like = current_customer.my_menu_likes.find_by(my_menu_id: @my_menu.id)
+    like.destroy
+    redirect_to request.referer
+  end
 
 end
