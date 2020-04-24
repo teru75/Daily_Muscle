fa97c797906e8866f2144bbc79bf87448de17026
teru75 <love.akg.75@gmail.com>
2020-04-24 07:45:39 +0000

    [update]follow, follow_count js

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,14 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_customer!
   def create
     current_customer.follow(params[:customer_id])
-    customer = Customer.find(params[:customer_id])
-    flash[:success] = "#{customer.name}をフォローしました。"
+    @customer = Customer.find(params[:customer_id])
+    flash[:success] = "#{@customer.name}をフォローしました。"
   end
 
   def destroy
     current_customer.unfollow(params[:customer_id])
-    customer = Customer.find(params[:customer_id])
-    flash[:notice] = "#{customer.name}のフォローを外しました。"
+    @customer = Customer.find(params[:customer_id])
+    flash[:notice] = "#{@customer.name}のフォローを外しました。"
   end
 
   def follower
