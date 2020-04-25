e3b2815673a8c01649abe8921efec034a5eca312
teru75 <love.akg.75@gmail.com>
2020-04-25 09:23:30 +0000

    [update]event-template is_enabled

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     @event = Event.where(created_at: Time.now.all_day)
+    @customer = Customer.where(created_at: Time.now.all_day)
   end
 end
--- spec/factories/event_templates.rb
+++ spec/factories/event_templates.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :event_template do
     part { Faker::Number.between(from: 0, to: 7) }
     theme { Faker::Lorem.characters(number:10) }
+    introduction { Faker::Lorem.characters(number:100) }
   end
 end 
