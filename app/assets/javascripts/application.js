b080ddf3652dffbb5ead6ae677bdb269e6f9a88e
teru75 <love.akg.75@gmail.com>
2020-04-27 08:00:29 +0000

    [update]bgswitcher path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,19 +22,7 @@
 //= require_tree .
 
 // トップ画面のスライドショーの設定
-$(window).load(function() {
-  $('.bg-slider').bgSwitcher({
-    images: [ 'assets/dread.jpg', 'assets/santoukin.jpg', 'assets/sideraise.jpg',  'assets/buttleroap.jpg', 'assets/burbel.jpg'], // 切り替える背景画像を指定
-    interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
-    loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
-    shuffle: false, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
-    effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
-    duration: 2000, // エフェクトの時間を指定します。
-    easing: "swing" // エフェクトのイージングをlinear,swingから指定
 
-  });
-
-});
 
 
 // 詳細画面と編集画面の切り替え
