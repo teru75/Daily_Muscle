93adaca5835e95c050b8c2ff288f18117ae16262
teru75 <love.akg.75@gmail.com>
2020-04-27 05:41:17 +0000

    [update]migration file fix

--- db/migrate/20200425092809_drop_my_menu_likes.rb
+++
@@ -1,8 +0,0 @@
-class DropMyMenuLikes < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :my_menu_likes do |t|
-      t.integer :customer_id
-      t.integer :my_menu_id
-    end
-  end
-end
