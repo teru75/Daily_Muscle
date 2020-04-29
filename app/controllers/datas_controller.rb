6c2a110ecdd2112cb80a845a93a53eb2cf9673e3
teru75 <love.akg.75@gmail.com>
2020-04-29 11:18:08 +0000

    [update]datapage-search_method

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -2,15 +2,18 @@ class DatasController < ApplicationController
   before_action :authenticate_customer!
   def index
     @customer = Customer.find(params[:customer_id])
-    if @customer.is_data_available == false
+    if @customer.is_data_available == false && @customer != current_customer
       flash[:alert] = "このアカウントのデータは許可されていません。"
       redirect_to customer_path(@customer)
     end
     @dayline = []
     @timeline = []
     count = 0
+    if params[:name].blank?
+    @events = @customer.events.joins(:menus).order(start: "ASC").last(5)
+    else
     @events = @customer.events.joins(:menus).where("menus.name == ?", params[:name]).order(start: "ASC").last(5)
-
+    end
     @events.each do |event|
       event.menus.each do |menu|
         if menu.name == params[:name] && count < 5
